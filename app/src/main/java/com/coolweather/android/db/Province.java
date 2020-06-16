5dd68dc766e5c6a5684ad7ce200a67bf7d4e9415
suancaiyuyoudiancai <499676385@qq.com>
2020-06-16 15:34:41 +0800

    完成遍历省市县三级列表的功能。

--- app/src/main/java/com/coolweather/android/db/Province.java
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -4,27 +4,33 @@ import org.litepal.crud.DataSupport;
 
 public class Province extends DataSupport {
 
-    private  int id;
-    private  String provinceName;
-    private  int provinceCode;
+    private int id;
 
-    public   int getId(){
-        return  id;
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
     }
 
-    public  void  setId(int id){
+    public void setId(int id) {
         this.id = id;
     }
-    public String getProvinceName(){
+
+    public String getProvinceName() {
         return provinceName;
     }
-    public void  setProvinceName(String provinceName){
+
+    public void setProvinceName(String provinceName) {
         this.provinceName = provinceName;
     }
-    public int getProvinceCode(){
+
+    public int getProvinceCode() {
         return provinceCode;
     }
-    public void setProvinceCode(int provinceCode){
+
+    public void setProvinceCode(int provinceCode) {
         this.provinceCode = provinceCode;
     }
 }
